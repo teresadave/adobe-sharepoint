6598268c09e709821e03d5d198be78b480db4d74
Teresadave <teresadave20@gmail>
2022-04-07 01:04:41 +0100

    first commitg

--- components/main.js
+++ components/main.js
@@ -98,7 +98,7 @@ const Main = () => {
                     
 
 
-                          {showModals ? (
+                          {showModal ? (
         <>
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
